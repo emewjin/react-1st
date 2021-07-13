5f8bc95522df7fa66386fbeb532961ee049b97c1
emewjin <yujinwork03@gmail.com>
2021-07-13 14:04:49 +0900

    Modify : review page message related with rating counts

--- src/pages/ReviewPage/ReviewPage.js
+++ src/pages/ReviewPage/ReviewPage.js
@@ -15,11 +15,26 @@ export default class ReviewPage extends Component {
       genreMovie: [],
       ratingsCount: 0,
       modalOpened: false,
+      isLoading: true,
     };
     this.scrollBoxRef = createRef();
   }
 
+  // checkLoading = (entries, observer) => {
+  //   entries.forEach(entry => {
+  //     //타겟 요소가 바뀔 부분
+  //   });
+  // };
+
   componentDidMount() {
+    // let scrollObserver = new IntersectionObserver(this.checkLoading, {
+    //   root: this.scrollBoxRef,
+    //   threshold: 0.5,
+    // });
+    // scrollObserver.observe(target);
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 5000);
     this.getMovieData();
     this.updateRatingCount();
     this.scrollBoxRef.current.addEventListener(
@@ -95,10 +110,28 @@ export default class ReviewPage extends Component {
     });
   };
 
+  getMessageByCount = countValue => {
+    let message;
+    Object.keys(MESSAGE).some(ranges => {
+      const rangeArr = ranges.split('-');
+      const [rangeA, rangeB] = rangeArr;
+      if (+countValue >= rangeA && +countValue <= (rangeB || rangeA)) {
+        message = MESSAGE[ranges];
+        return true;
+      }
+    });
+    return message;
+  };
+
   render() {
     const { movieData, ratingsCount, modalOpened } = this.state;
-    const { getMovieData, openFilterGenre, updateRatingCount, closeModal } =
-      this;
+    const {
+      getMovieData,
+      openFilterGenre,
+      updateRatingCount,
+      closeModal,
+      getMessageByCount,
+    } = this;
     return (
       <>
         {modalOpened && (
@@ -115,9 +148,7 @@ export default class ReviewPage extends Component {
         <section className="reviewSection">
           <header className="reviewHeader">
             <h2 className="reviewCount">{ratingsCount}</h2>
-            <h3 className="reviewNotice">
-              이제 알듯 말듯 하네요. 조금만 더 평가해주세요!
-            </h3>
+            <h3 className="reviewNotice">{getMessageByCount(ratingsCount)}</h3>
             <div className="reviewMenu">
               <ul>
                 {CATEGORY_LIST.map(category => (
@@ -170,3 +201,12 @@ const CATEGORY_LIST = [
     name: '책',
   },
 ];
+
+const MESSAGE = {
+  '0-10': '재미있게 보셨던 영화를 알려주세요',
+  '11-30': '아주 조금 알 것 같아요',
+  '31-50': '당신의 평가로 추천이 더 정확해지고 있어요!',
+  '51-80': '정말 영화를 좋아하시는군요!',
+  '81-99': '100개 돌파가 얼마 남지 않았어요!',
+  '100-150': '이렇게 많이 영화평을 공유해주셔서 고마워요',
+};
